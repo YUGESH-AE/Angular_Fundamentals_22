e2dda7b978d97346ad57bc4ca52c32fecb043568
YUGESH-AE <yugeshae@gmail.com>
2026-08-09 11:22:30 +0530

    added change detection stratergy

--- src/app/api/get-api/get-api.ts
+++ src/app/api/get-api/get-api.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Users} from '../Users';
@@ -14,13 +14,14 @@ export class GetApi {
 
   users:Users[]=[];
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,private cdr:ChangeDetectorRef) {
   }
 
   getUsers():void{
     this.http.get<Users[]>("https://jsonplaceholder.typicode.com/users")
       .subscribe((results:any)=>{
         this.users=results;
+        this.cdr.detectChanges();
       })
   }
 }
